907a1427b13d5cd945e602afd74b99f172a52476
Armando <armandoramossanchez@gmail.com>
2021-09-21 12:58:57 +0200

    subo reto 9

---
+++ Reto 9/js/script.js
@@ -0,0 +1,23 @@
+//Reto 9
+
+/*
+Muestra los números del 1 al 100 (ambos incluidos) divisibles entre 2 y 3. Utiliza
+el bucle que desees.
+*/
+
+/*
+Autor: Armando Ramos
+Version: 1.0
+*/
+
+//Input
+
+//Proceso
+
+//Output
+
+for (let i = 1; i <= 100; i++) {
+    if (i%2 == 0 && i%3 == 0) {
+        console.log("El número " + i + " es divisible entre 2 y 3.");
+    }
+}
