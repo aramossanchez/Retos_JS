9f7015237eb4c12ccaddc31adf9df5b5f14d3020
Armando <armandoramossanchez@gmail.com>
2021-09-21 12:55:33 +0200

    subo reto 8

---
+++ Reto 8/js/script.js
@@ -0,0 +1,20 @@
+//Reto 8
+
+/*
+Haz el mismo ejercicio anterior con un bucle for.
+*/
+
+/*
+Autor: Armando Ramos
+Version: 1.0
+*/
+
+//Input
+
+//Proceso
+
+//Output
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
