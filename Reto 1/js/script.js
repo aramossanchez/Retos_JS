ee110640275812661530d326729ce000ca68e89d
Armando <armandoramossanchez@gmail.com>
2021-09-21 12:27:03 +0200

    subo reto 1

---
+++ Reto 1/js/script.js
@@ -0,0 +1,28 @@
+//Reto 1
+/*
+Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor
+de los dos. Si son iguales indicarlo también. Ves cambiando los valores para
+comprobar que funciona.
+*/
+
+/*
+Autor: Armando Ramos
+Version: 1.0
+*/
+
+//Input
+
+var a = 15;
+var b = 5;
+
+//Proceso
+
+if (a > b) {
+    console.log(a + " es el número mayor.");    
+} else if (a < b){
+    console.log(b + " es el número mayor.");    
+} else{
+    console.log(a + " y " + b + " son iguales.");
+}
+
+//Output
