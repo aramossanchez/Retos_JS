3e501be62e9ec699efe10091ffa95ae5752aac9f
Armando <armandoramossanchez@gmail.com>
2021-09-21 13:11:54 +0200

    subo reto 10

---
+++ Reto 10/js/script.js
@@ -0,0 +1,37 @@
+//Reto 10
+
+/*
+Realiza una aplicación que nos pida un número de ventas a introducir, después
+nos pedirá tantas ventas por teclado como número de ventas se hayan indicado.
+Al final mostrara la suma de todas las ventas. Piensa que es lo que se repite y lo
+que no.
+*/
+
+/*
+Autor: Armando Ramos
+Version: 1.0
+*/
+
+//Input
+
+var cantidadVentas = prompt("Número de ventas.");
+var precios = [];
+var precioTotal = 0;
+
+//Proceso
+
+cantidadVentas = parseInt(cantidadVentas);
+
+for (let i = 1; i <= cantidadVentas; i++) {
+    let precio = prompt("Introduce el precio del artículo " + i);
+    precio = parseFloat(precio);
+    precios.push(precio);
+};
+console.log(precios);
+precios.forEach(precio => {
+    precioTotal += precio;
+});
+
+//Output
+
+console.log("El precio total es " + precioTotal);
