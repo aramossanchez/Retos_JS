514fa0a103d77df0aefdf315531d285ef9fcddaf
Armando <armandoramossanchez@gmail.com>
2021-09-21 12:53:09 +0200

    subo reto 7

---
+++ Reto 7/js/script.js
@@ -0,0 +1,23 @@
+//Reto 7
+
+/*
+Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.
+*/
+
+/*
+Autor: Armando Ramos
+Version: 1.0
+*/
+
+//Input
+
+var contador = 1;
+
+//Proceso
+
+//Output
+
+while (contador <=100) {
+    console.log(contador);
+    contador++;    
+}
