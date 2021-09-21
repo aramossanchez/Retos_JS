58157dd845ef09a40b8bad026127595e7fd2b9dd
Armando <armandoramossanchez@gmail.com>
2021-09-21 12:29:41 +0200

    subo reto 2

--- Reto 1/js/script.js
+++ Reto 1/js/script.js
@@ -1,4 +1,5 @@
 //Reto 1
+
 /*
 Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor
 de los dos. Si son iguales indicarlo también. Ves cambiando los valores para
---
+++ Reto 2/js/script.js
@@ -0,0 +1,22 @@
+//Reto 2
+
+/*
+Declara un String que contenga tu nombre, después muestra un mensaje de
+bienvenida por consola. Por ejemplo: si introduzco “Fernando”, me aparezca
+“Bienvenido Fernando”.
+*/
+
+/*
+Autor: Armando Ramos
+Version: 1.0
+*/
+
+//Input
+
+var nombre = "Armando";
+
+//Proceso
+
+//Output
+
+console.log("Bienvenido " + nombre);
