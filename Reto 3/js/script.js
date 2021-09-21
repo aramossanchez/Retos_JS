4b4c90e234e438737a71118efefe309155fd548d
Armando <armandoramossanchez@gmail.com>
2021-09-21 12:32:08 +0200

    subo reto 3

---
+++ Reto 3/js/script.js
@@ -0,0 +1,21 @@
+//Reto 3
+
+/*
+Modifica la aplicación anterior, para que nos pida el nombre que queremos
+introducir (recuerda usar prompt).
+*/
+
+/*
+Autor: Armando Ramos
+Version: 1.0
+*/
+
+//Input
+
+var nombre = prompt("Introduce tu nombre");
+
+//Proceso
+
+//Output
+
+console.log("Bienvenido " + nombre);
